b1d3c39def891ab240339aca503f5d15ca1f76a1
sitegui <sitegui@sitegui.com.br>
2013-10-24 15:02:10 -0200

    Completed economy values

--- splendens/splendens/Economy.m
+++ splendens/splendens/Economy.m
@@ -107,40 +107,36 @@
 	return -1;
 }
 
-//--------falta fazer-----------
 + (int)upgradePopulationCostForType:(CellType)type level:(int)level {
-	if (type == CellTypeBasic) return 10;
-	if (type == CellTypeCity && level == 1) return 30;
-	if (type == CellTypeCity && level == 2) return 45;
-	if (type == CellTypeCity && level == 3) return 60;
-	if (type == CellTypeCity && level == 4) return 75;
-	if (type == CellTypeTower && level == 1) return 20;
-	if (type == CellTypeTower && level == 2) return 30;
-	if (type == CellTypeTower && level == 3) return 40;
-	if (type == CellTypeTower && level == 4) return 50;
-	if (type == CellTypeLab && level == 1) return 20;
-	if (type == CellTypeLab && level == 2) return 30;
-	if (type == CellTypeLab && level == 3) return 40;
-	if (type == CellTypeLab && level == 4) return 50;
+	if (type == CellTypeCity && level == 1) return 5;
+	if (type == CellTypeCity && level == 2) return 15;
+	if (type == CellTypeCity && level == 3) return 25;
+	if (type == CellTypeCity && level == 4) return 35;
+	if (type == CellTypeTower && level == 1) return 10;
+	if (type == CellTypeTower && level == 2) return 10;
+	if (type == CellTypeTower && level == 3) return 20;
+	if (type == CellTypeTower && level == 4) return 30;
+	if (type == CellTypeLab && level == 1) return 10;
+	if (type == CellTypeLab && level == 2) return 10;
+	if (type == CellTypeLab && level == 3) return 20;
+	if (type == CellTypeLab && level == 4) return 30;
 	return -1;
-	return 0;
 }
-+ (int)upgradePopulationForType:(CellType)type level:(int)level {
-	if (type == CellTypeBasic) return 10;
-	if (type == CellTypeCity && level == 1) return 30;
-	if (type == CellTypeCity && level == 2) return 45;
-	if (type == CellTypeCity && level == 3) return 60;
-	if (type == CellTypeCity && level == 4) return 75;
-	if (type == CellTypeTower && level == 1) return 20;
-	if (type == CellTypeTower && level == 2) return 30;
-	if (type == CellTypeTower && level == 3) return 40;
-	if (type == CellTypeTower && level == 4) return 50;
-	if (type == CellTypeLab && level == 1) return 20;
-	if (type == CellTypeLab && level == 2) return 30;
-	if (type == CellTypeLab && level == 3) return 40;
-	if (type == CellTypeLab && level == 4) return 50;
+
++ (int)upgradeManaCostForType:(CellType)type level:(int)level {
+	if (type == CellTypeCity && level == 1) return 0;
+	if (type == CellTypeCity && level == 2) return 2;
+	if (type == CellTypeCity && level == 3) return 4;
+	if (type == CellTypeCity && level == 4) return 6;
+	if (type == CellTypeTower && level == 1) return 0;
+	if (type == CellTypeTower && level == 2) return 3;
+	if (type == CellTypeTower && level == 3) return 6;
+	if (type == CellTypeTower && level == 4) return 9;
+	if (type == CellTypeLab && level == 1) return 0;
+	if (type == CellTypeLab && level == 2) return 4;
+	if (type == CellTypeLab && level == 3) return 8;
+	if (type == CellTypeLab && level == 4) return 12;
 	return -1;
-	return 0;
 }
 
 @end
