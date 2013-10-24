c0a846b3126f1ab743787f59111bdf5950a31ed6
sitegui <sitegui@sitegui.com.br>
2013-10-24 11:53:40 -0200

    Added Economy class

---
+++ splendens/splendens/Economy.h
@@ -0,0 +1,25 @@
+//
+//  Economy.h
+//  splendens
+//
+//  Created by Guilherme Souza on 10/24/13.
+//  Copyright (c) 2013 Codeca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Cell.h"
+
+@interface Economy : NSObject
+
++ (int)productionForType:(CellType)type level:(int)level;
++ (int)maxPopulationForType:(CellType)type level:(int)level;
++ (int)speedForType:(CellType)type level:(int)level;
++ (int)armorForType:(CellType)type level:(int)level;
++ (int)attackSpeedForType:(CellType)type level:(int)level;
++ (int)attackDamageForType:(CellType)type level:(int)level;
++ (int)attackRangeForType:(CellType)type level:(int)level;
++ (int)bonusMaxManaForType:(CellType)type level:(int)level;
++ (int)upgradePopulationCostForType:(CellType)type level:(int)level;
++ (int)upgradePopulationForType:(CellType)type level:(int)level;
+
+@end
---
+++ splendens/splendens/Economy.m
@@ -0,0 +1,44 @@
+//
+//  Economy.m
+//  splendens
+//
+//  Created by Guilherme Souza on 10/24/13.
+//  Copyright (c) 2013 Codeca. All rights reserved.
+//
+
+#import "Economy.h"
+
+@implementation Economy
+
++ (int)productionForType:(CellType)type level:(int)level {
+	return 0;
+}
++ (int)maxPopulationForType:(CellType)type level:(int)level {
+	return 0;
+}
++ (int)speedForType:(CellType)type level:(int)level {
+	return 0;
+}
++ (int)armorForType:(CellType)type level:(int)level {
+	return 0;
+}
++ (int)attackSpeedForType:(CellType)type level:(int)level {
+	return 0;
+}
++ (int)attackDamageForType:(CellType)type level:(int)level {
+	return 0;
+}
++ (int)attackRangeForType:(CellType)type level:(int)level {
+	return 0;
+}
++ (int)bonusMaxManaForType:(CellType)type level:(int)level {
+	return 0;
+}
++ (int)upgradePopulationCostForType:(CellType)type level:(int)level {
+	return 0;
+}
++ (int)upgradePopulationForType:(CellType)type level:(int)level {
+	return 0;
+}
+
+@end
