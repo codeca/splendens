87ad79f880d6952e065015e6503705bfde0453d8
Guilherme Souza <sitegui@sitegui.com.br>
2013-10-24 12:16:17 -0200

    Added some economy values

--- splendens/splendens/Economy.m
+++ splendens/splendens/Economy.m
@@ -11,10 +11,32 @@
 @implementation Economy
 
 + (int)productionForType:(CellType)type level:(int)level {
-	return 0;
+	if (type == CellTypeBasic) return 1;
+	if (type == CellTypeCity && level == 1) return 2;
+	if (type == CellTypeCity && level == 2) return 3;
+	if (type == CellTypeCity && level == 3) return 5;
+	if (type == CellTypeCity && level == 4) return 8;
+	if (type == CellTypeLab && level == 1) return 1;
+	if (type == CellTypeLab && level == 2) return 2;
+	if (type == CellTypeLab && level == 3) return 3;
+	if (type == CellTypeLab && level == 4) return 4;
+	return -1;
 }
 + (int)maxPopulationForType:(CellType)type level:(int)level {
-	return 0;
+	if (type == CellTypeBasic) return 10;
+	if (type == CellTypeCity && level == 1) return 30;
+	if (type == CellTypeCity && level == 2) return 45;
+	if (type == CellTypeCity && level == 3) return 60;
+	if (type == CellTypeCity && level == 4) return 75;
+	if (type == CellTypeTower && level == 1) return 20;
+	if (type == CellTypeTower && level == 2) return 30;
+	if (type == CellTypeTower && level == 3) return 40;
+	if (type == CellTypeTower && level == 4) return 50;
+	if (type == CellTypeLab && level == 1) return 20;
+	if (type == CellTypeLab && level == 2) return 30;
+	if (type == CellTypeLab && level == 3) return 40;
+	if (type == CellTypeLab && level == 4) return 50;
+	return -1;
 }
 + (int)speedForType:(CellType)type level:(int)level {
 	return 0;
