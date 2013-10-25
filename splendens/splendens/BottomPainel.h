be13e5c7433f8260f01ef81c33929f1d2368e376
rodolfobitu <rodolfobitu@MacBook-Pro-de-Rodolfo.local>
2013-10-25 13:28:07 -0200

    BottomPainel class created

---
+++ splendens/splendens/BottomPainel.h
@@ -0,0 +1,13 @@
+//
+//  BottomPainel.h
+//  splendens
+//
+//  Created by Rodolfo Bitu on 25/10/13.
+//  Copyright (c) 2013 Codeca. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface BottomPainel : SKSpriteNode
+
+@end
---
+++ splendens/splendens/BottomPainel.m
@@ -0,0 +1,13 @@
+//
+//  BottomPainel.m
+//  splendens
+//
+//  Created by Rodolfo Bitu on 25/10/13.
+//  Copyright (c) 2013 Codeca. All rights reserved.
+//
+
+#import "BottomPainel.h"
+
+@implementation BottomPainel
+
+@end
