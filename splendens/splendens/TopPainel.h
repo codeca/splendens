dde2842622ece15a1423bd5bbff4cef294c41e0f
rodolfobitu <rodolfobitu@gmail.com>
2013-10-30 11:46:29 -0200

    TopPainel class created

---
+++ splendens/splendens/TopPainel.h
@@ -0,0 +1,13 @@
+//
+//  TopPainel.h
+//  splendens
+//
+//  Created by Rodolfo Bitu on 30/10/13.
+//  Copyright (c) 2013 Codeca. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface TopPainel : SKSpriteNode
+
+@end
---
+++ splendens/splendens/TopPainel.m
@@ -0,0 +1,13 @@
+//
+//  TopPainel.m
+//  splendens
+//
+//  Created by Rodolfo Bitu on 30/10/13.
+//  Copyright (c) 2013 Codeca. All rights reserved.
+//
+
+#import "TopPainel.h"
+
+@implementation TopPainel
+
+@end
