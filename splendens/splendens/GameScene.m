f19c3547bdeea6c22da116476ed992d58184e3ad
sitegui <sitegui@sitegui.com.br>
2013-11-14 15:22:21 -0200

    Clear planned powers at the end of the turn

--- splendens/splendens/GameScene.m
+++ splendens/splendens/GameScene.m
@@ -196,6 +196,11 @@
 }
 
 - (void)applyPowers {
+	// Clear all powers overlay
+	for (Cell* cell in self.map.cells) {
+		[cell clearPowerOverlay];
+	}
+	
 	// Apply all powers, ordered by player
 	for (Player* player in self.players) {
 		for (NSDictionary* action in player.usedPowers) {
